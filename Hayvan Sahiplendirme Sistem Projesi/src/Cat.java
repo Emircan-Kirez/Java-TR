3b535439b774814be52a3708ab66a99eedca2711
Emircan KİREZ - Ch3rry <73709289+Emircan-Kirez@users.noreply.github.com>
2022-07-19 17:33:43 +0300

    Delete Hayvan Sahiplendirme Sistem Projesi directory

--- Hayvan Sahiplendirme Sistem Projesi/src/Cat.java
+++
@@ -1,5 +0,0 @@
-public class Cat extends Animal{
-    public Cat(int age, String kind, String healthStatus, String foundDate, int foodItAte){
-        super(age, kind, healthStatus, foundDate, foodItAte);
-    }
-}
--- Hayvan Sahiplendirme Sistem Projesi/src/Dog.java
+++
@@ -1,5 +0,0 @@
-public class Dog extends Animal{
-    public Dog(int age, String kind, String healthStatus, String foundDate, int foodItAte){
-        super(age, kind, healthStatus, foundDate, foodItAte);
-    }
-}
