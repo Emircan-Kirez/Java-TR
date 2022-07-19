973a0a99f5388f5e22d8402176d48f4cb593f6e1
Emircan KİREZ - Ch3rry <73709289+Emircan-Kirez@users.noreply.github.com>
2022-07-19 17:19:41 +0300

    Add files via upload

---
+++ Hayvan Sahiplendirme Sistem Projesi/src/Cat.java
@@ -0,0 +1,5 @@
+public class Cat extends Animal{
+    public Cat(int age, String kind, String healthStatus, String foundDate, int foodItAte){
+        super(age, kind, healthStatus, foundDate, foodItAte);
+    }
+}
---
+++ Hayvan Sahiplendirme Sistem Projesi/src/Dog.java
@@ -0,0 +1,5 @@
+public class Dog extends Animal{
+    public Dog(int age, String kind, String healthStatus, String foundDate, int foodItAte){
+        super(age, kind, healthStatus, foundDate, foodItAte);
+    }
+}
